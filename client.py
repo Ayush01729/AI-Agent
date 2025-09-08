00ade1be0a03c9f87fca4f5e63a20ab8131c63fb
Ayush01729 <76625106+Ayush01729@users.noreply.github.com>
2025-09-08 14:19:03 +0000

    minor changes

--- client.py
+++ client.py
@@ -7,6 +7,6 @@ load_dotenv()
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.0-flash", 
-    google_api_key=os.getenv("GOOGLE_API_KEY")
+    google_api_key= "AIzaSyApRZgsB_iqI-yjT_CXy2hBBCfh0pPBezg"
 )
 
