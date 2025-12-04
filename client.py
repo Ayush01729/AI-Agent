a75d1d04d14c6466287d7c77f420dcddd8d28e47
Ayush01729 <76625106+Ayush01729@users.noreply.github.com>
2025-12-04 22:08:08 +0530

    api key hidden

--- client.py
+++ client.py
@@ -1,12 +1,13 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 import os
 from dotenv import load_dotenv
+import streamlit as st
 
 
 load_dotenv()
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash", 
-    google_api_key= "AIzaSyDQrN3Df0dqPQ-Wu0sPuD8Os_h6X2Sq5rY"
+    google_api_key= st.secrets["API_KEY"]
 )
 
