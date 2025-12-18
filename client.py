67f8f4f23fb2a833b67659d45c7160d1e29e4fc8
Ayush01729 <76625106+Ayush01729@users.noreply.github.com>
2025-12-18 18:58:39 +0530

    secret changes

--- client.py
+++ client.py
@@ -8,6 +8,6 @@ load_dotenv()
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash", 
     # google_api_key=os.getenv("API_KEY")
-    google_api_key="AIzaSyAo_uxi8_QjDFn1FJeWqPYlQDGRx24taf0"  # Replace with your actual API key
+    google_api_key=os.getenv("API_KEY")  # Replace with your actual API key
 )
 
